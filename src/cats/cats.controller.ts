b2551f357265bfa7ebdf3f34dd4f215db2bfa0d4
mmrakt <mimuraakkun3@gmail.com>
2022-08-31 23:35:51 +0900

    Add: job validation pipe

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get, Post, Body, UseFilters } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UseFilters,
+  Param,
+  ParseIntPipe,
+  UsePipes,
+} from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
-import { ForbiddenException } from '../forbidden.exception';
 import { HttpExceptionFilter } from '../http-exception.filter';
+import { JoiValidationPipe } from '../validation.pipe';
 
 @Controller('cats')
 export class CatsController {
@@ -11,6 +20,7 @@ export class CatsController {
 
   @Post()
   @UseFilters(new HttpExceptionFilter())
+  @UsePipes(new JoiValidationPipe(createCatSchema))
   async create(@Body() createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
   }
@@ -18,5 +28,10 @@ export class CatsController {
   @Get()
   async findAll(): Promise<Cat[]> {
     return this.catsService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.catsService.findOne(id);
   }
 }
