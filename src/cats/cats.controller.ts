49b51c59f77bc3832b1e1080f3331a660e72cfd4
mmrakt <mimuraakkun3@gmail.com>
2022-09-02 21:21:21 +0900

    Add roles guard

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -6,19 +6,23 @@ import {
   UseFilters,
   Param,
   ParseIntPipe,
+  UseGuards,
+  SetMetadata,
 } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
-import { HttpExceptionFilter } from '../http-exception.filter';
 import { ValidationPipe } from '../validation.pipe';
+import { RolesGuard } from '../roles.guard';
+import { Roles } from 'src/roles.decorator';
 
 @Controller('cats')
+@UseGuards(new RolesGuard())
 export class CatsController {
   constructor(private catsService: CatsService) {}
 
   @Post()
-  @UseFilters(new HttpExceptionFilter())
+  @Roles('admin')
   async create(@Body(new ValidationPipe()) createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
   }
